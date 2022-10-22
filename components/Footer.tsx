2bc9a89a4b954027c900c5ab71a497109e7b7116
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2022-10-21 22:22:52 -0300

    ecommerce SPA to understand nextjs with snipcart

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Next.js app with a&nbsp;<a href="<https://snipcart.com>">Snipcar t</a>
+        &nbsp;- powered store
+      </p>
+      <div>
+        <a href="<https://github.com/snipcart/snipcart-nextjs>-spa">Github</a>
+      </div>
+    </footer>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="app">
+        <Header/>
+        <Component {...pageProps} />
+        <Footer/>
+    </div>
 }
 
-export default MyApp
+export default MyApp;
